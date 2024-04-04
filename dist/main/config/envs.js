d4514ce4b8f63d5955f388bb4fd1862629b5984b
Isabella Santiago <isabella.-.bela@hotmail.com>
2024-04-04 19:29:36 -0300

    config: deploy

--- dist/main/config/envs.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.envs = void 0;
-exports.envs = {
-    port: process.env.PORT ?? '8081',
-    secret_jwt_key: process.env.JWT_SECRET_KEY ?? 'mysecret'
-};
--- dist/main/routes/signup-routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_route_adapter_1 = require("../adapters/express-route-adapter");
-const singup_1 = require("../factories/singup");
-function default_1(router) {
-    router.post('/', (0, express_route_adapter_1.adaptRoute)((0, singup_1.makeSignupController)()));
-}
-exports.default = default_1;
